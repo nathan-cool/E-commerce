4c47f337eb5488706b23db792f2d14dd7c0af006
Nathan <127421398+nathan-cool@users.noreply.github.com>
2024-08-12 22:39:21 +0100

    Updated styles.css for home page

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = ['static/']
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 
 MEDIA_URL = '/images/'
