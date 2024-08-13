2f3933f223dde5f8d632e65350ee1931fc33808a
Nathan <127421398+nathan-cool@users.noreply.github.com>
2024-08-13 11:38:09 +0100

    Update URL configuration to include about page. Added some html to the about page

--- store/urls.py
+++ store/urls.py
@@ -1,6 +1,9 @@
+from django import views
 from django.urls import path # type: ignore
 from .views import home
+from . import views
 
 urlpatterns = [
     path('', home, name='home'),
+    path('about/', views.about, name='about'),
 ]
