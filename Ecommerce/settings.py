25210e6bd1447708a7961a82699ceff3b529fb4b
Nathan <127421398+nathan-cool@users.noreply.github.com>
2024-08-27 19:24:20 +0100

    Update settings.py, urls.py, for adding checkout functionality

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'store',
     'authentication',
     'cart',
+    'checkout',
 ]
 
 MIDDLEWARE = [
--- Ecommerce/urls.py
+++ Ecommerce/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('', include('store.urls')),
     path('', include('authentication.urls')),
     path('cart/', include('cart.urls')),
+    path('checkout', include('checkout.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
