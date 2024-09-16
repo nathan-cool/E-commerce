108245e9c744806761a510400d1844022dcc2677
Nathan <127421398+nathan-cool@users.noreply.github.com>
2024-09-16 14:42:21 +0100

    Updated URL for stripe

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
-from . import views
-from checkout import views
+from . import views as app_views
+from checkout import views as checkout_views
 
 urlpatterns = [
-    path('', views.checkout, name='checkout'),
-    path('success/', views.payment_success, name='payment_success'),
-    path('cancelled/', views.payment_cancelled, name='payment_cancelled'),
-    path('stripe-webhook/', views.stripe_webhook, name='stripe_webhook'),
-    
-
+    path('', app_views.checkout, name='checkout'),
+    path('success/', app_views.payment_success, name='payment_success'),
+    path('cancelled/', app_views.payment_cancelled, name='payment_cancelled'),
+    path('stripe-webhook/', checkout_views.stripe_webhook, name='stripe_webhook'),
 ]
