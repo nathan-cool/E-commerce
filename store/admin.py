b220495b82db65cd542ac7d8db2e2dcf5ac7a017
Nathan <127421398+nathan-cool@users.noreply.github.com>
2024-10-01 11:37:28 +0100

    Update admin.py for pep8 and comments

--- store/admin.py
+++ store/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Product, Category, Customer, Order
 
-# Register your models here.
+# Models for backend admin
 admin.site.register(Product)
 admin.site.register(Category)
 admin.site.register(Customer)
