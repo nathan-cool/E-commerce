6a5c8023d731aafa2d3a62166214a04a84fcb8a3
Nathan <127421398+nathan-cool@users.noreply.github.com>
2024-11-15 19:04:23 +0000

    Fixed type in settings.py

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -161,7 +161,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-SSTATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
